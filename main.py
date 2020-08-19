734662ba961a5817627ba4298061d6ccc670db3b
Vishwa chauhan <vishwa.chauhan_cs18@gla.ac.in>
2020-08-20 01:36:07 +0530

    adding sound effects
    Signed-off-by: Vishwa chauhan <vishwa.chauhan_cs18@gla.ac.in>

--- main.py
+++ main.py
@@ -39,3 +39,12 @@ if __name__ == "__main__":
      )
 
 # game sounds
+
+GAME_SOUNDS['die']=pygame.mixer.sounds("gallery/audio/die.wav")
+GAME_SOUNDS['hit']=pygame.mixer.sounds("gallery/audio/hit.wav")
+GAME_SOUNDS['point']=pygame.mixer.sounds("gallery/audio/point.wav")
+GAME_SOUNDS['swoosh']=pygame.mixer.sounds("gallery/audio/swoosh.wav")
+GAME_SOUNDS['wing']=pygame.mixer.sounds("gallery/audio/wing.wav")
+
+GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha()
+GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert()
